e6e870270b8d1c35b2114b5d47e99d816275b0af
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-03-28 17:40:39 -0400

    bspwm now has alacritty as pseudo_tiled

--- .config/bspwm/polybar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if $(pgrep -u $UID -x polybar >/dev/null); then
-    killall -q polybar
-    while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-fi
-
-"$HOME/.config/polybar/grayblocks/launch.sh"
