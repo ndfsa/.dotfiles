bdfb5598dcb64df79bd1ae517a32d9e78adee545
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-08-01 20:53:24 -0400

    Update functions.zsh

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,16 +1,11 @@
-## Add sudo to the start of the command
+## Add doas to the start of the command
 sudo-command-line() {
     [[ -z $BUFFER ]] && zle up-history
-    if [[ $BUFFER == sudo\ * ]]; then
-        LBUFFER="${LBUFFER#sudo }"
-    elif [[ $BUFFER == sudoedit\ * ]]; then
-        LBUFFER="vim ${LBUFFER#sudoedit }"
-    elif [[ $BUFFER == vim\ * ]]; then
-        LBUFFER="sudoedit ${LBUFFER#vim }"
+    if [[ $BUFFER == doas\ * ]]; then
+        LBUFFER="${LBUFFER#doas }"
     else
-        LBUFFER="sudo $LBUFFER"
+        LBUFFER="doas $LBUFFER"
     fi
 }
 
 zle -N sudo-command-line
-
