31fe04bf079ca31a3610a582710f8324a08feade
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-11-23 23:49:20 -0400

    flameshot and others

--- .config/nvim/lua/cmp-config.lua
+++ .config/nvim/lua/cmp-config.lua
@@ -7,7 +7,7 @@ cmp.setup {
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm({
+		['<C-y>'] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Insert,
 			select = true,
 		})
@@ -18,10 +18,10 @@ cmp.setup {
 		end
 	},
 	sources = {
-		{ name = 'buffer' },
 		{ name = 'nvim_lsp' },
-		{ name = 'path' },
 		{ name = 'luasnip' },
+		{ name = 'path' },
 		{ name = 'orgmode' },
+		{ name = 'buffer' },
 	},
 }
