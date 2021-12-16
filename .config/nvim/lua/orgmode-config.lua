e37d75aadcdb93302830cc7cacd3c1c3b7393243
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-12-16 02:05:36 -0400

    noindent for orgmode

--- .config/nvim/lua/orgmode-config.lua
+++ .config/nvim/lua/orgmode-config.lua
@@ -2,5 +2,6 @@ require('orgmode').setup({
 	org_agenda_files = {'~/Documents/org/*'},
 	org_default_notes_file = '~/Documents/org/notes.org',
 	org_agenda_templates = {t = { description = 'Task', template = '* TODO %?\n' }},
-	org_todo_keywords = {'TODO', 'NEXT', '|', 'DONE'}
+	org_todo_keywords = {'TODO', 'NEXT', '|', 'DONE'},
+	org_indent_mode = 'noindent'
 })
