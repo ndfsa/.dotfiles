f680fef55297e1d60595d020942f0f91e1df2bd3
ndfsa <25588077+ndfsa@users.noreply.github.com>
2021-12-23 21:52:38 -0400

    changes

--- .config/nvim/lua/plugins/filetype-config.lua
+++ .config/nvim/lua/plugins/filetype-config.lua
@@ -2,9 +2,11 @@ require("filetype").setup({
     overrides = {
         extensions = {
 			service = "systemd",
+			target = "systemd",
 			timer = "systemd"
         },
         shebang = {
+            sh = "sh",
             dash = "sh",
             bash = "bash",
         },
