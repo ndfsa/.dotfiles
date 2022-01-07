6bc6b88eec1f8e4b84c22d8db3c3a4fe6bd12f0d
ndfsa <25588077+ndfsa@users.noreply.github.com>
2022-01-06 21:30:26 -0400

    which key is really cool

--- .config/nvim/lua/plugins/which-key-config.lua
+++ .config/nvim/lua/plugins/which-key-config.lua
@@ -1,1 +1,62 @@
-require('which-key').setup()
+local wk = require('which-key')
+
+wk.setup()
+
+wk.register({
+    b = {
+        name = 'Buffer',
+        n = 'Next',
+        p = 'Previous',
+        d = 'Delete',
+        f = 'Find',
+        z = 'Fuzzy',
+    },
+    s = {
+        name = 'Set',
+        n = 'Relative numbers',
+        w = 'Wrap lines',
+        s = 'Spell',
+        c = 'Colorizer',
+        l = 'Indent guide lines',
+    },
+    f = {
+        name = 'Files',
+        s = 'Save',
+        f = 'Find',
+        g = 'Grep',
+    },
+    o = {
+        name = 'Open',
+        e = 'Explorer',
+        p = 'Projects',
+        E = 'Lir Explorer',
+        m = 'Media files',
+    },
+    d = {
+        name = 'Debug',
+        b = 'Toggle breakpoint',
+        r = 'Run debugger',
+        k = 'Continue debugger',
+        l = 'Run last debugger',
+        c = 'Stop debugger',
+        o = 'Step over',
+        i = 'Step into',
+        t = 'Step out',
+        j = 'Run to cursor',
+        u = 'Toggle UI',
+        f = {
+            name = 'Find',
+            p = 'Commands',
+            b = 'Breakpoints',
+            v = 'Variables',
+            f = 'Frames',
+        },
+    },
+    g = {
+        name = 'Git',
+        b = 'Branches',
+        s = 'Status',
+        w = 'Line blame',
+        W = 'Pop Up blame',
+    }
+}, {prefix = '<leader>'})
