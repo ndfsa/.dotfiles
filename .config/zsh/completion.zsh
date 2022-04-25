38771113f6bf38f686a3d1b9159359698d260bf9
ndfsa <25588077+ndfsa@users.noreply.github.com>
2022-04-25 18:33:35 -0400

    changes to completion in zsh

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,11 +1,9 @@
 autoload -U compinit && compinit
 
 zstyle ':completion:*' menu select=1
-zstyle ':completion:*' matcher-list \
-    'm:{a-z}={A-Z}'
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' file-list all
 zstyle ':completion:*' complete-options true
 zstyle ':completion:alias-expension:*' completer _expand_alias
 zstyle ':completion:*:*:kill:*' menu yes select
--- bin/normalize_images.sh
+++ bin/normalize_images.sh
@@ -2,15 +2,9 @@
 
 mkdir normalized
 parallel -j 6 magick mogrify \
-    -define png:compression-level=9 \
-    -format png \
+    -define webp:lossless=true \
+    -format webp \
     -alpha off \
-    -strip \
-    -colorspace RGB \
-    -filter Cubic \
-    -define filter:B=0.33 \
-    -distort Resize "2560x1440^" \
-    -colorspace sRGB \
     -path ./normalized \
     ::: ${@}
 cd normalized
