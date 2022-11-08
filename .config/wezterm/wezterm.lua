f85dbc3382923ce95e217ed4f021f3f38ba53185
ndfsa <25588077+ndfsa@users.noreply.github.com>
2022-11-07 20:21:30 -0400

    no ligatures

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,22 +11,11 @@ local function rule(intensity, weight, italic)
                 italic = italic,
                 weight = weight,
             },
-            harfbuzz_features = nil,
             'Symbols Nerd Font',
             'Symbola',
         }),
     }
 end
---
-wezterm.on('toggle-ligature', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-    if not overrides.harfbuzz_features then
-        overrides.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-    else
-        overrides.harfbuzz_features = nil
-    end
-    window:set_config_overrides(overrides)
-end)
 return {
     font_rules = {
         rule('Half', 'ExtraLight', false),
@@ -36,6 +25,7 @@ return {
         rule('Normal', 'Regular', true),
         rule('Bold', 'ExtraBold', true),
     },
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
     use_fancy_tab_bar = false,
     initial_rows = 38,
     initial_cols = 120,
@@ -124,11 +114,6 @@ return {
             key = 't',
             mods = 'ALT',
             action = act.SpawnTab('CurrentPaneDomain'),
-        },
-        {
-            key = 'l',
-            mods = 'ALT',
-            action = act.EmitEvent('toggle-ligature'),
         },
     },
 }
