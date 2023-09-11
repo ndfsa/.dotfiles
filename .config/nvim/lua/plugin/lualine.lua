25a2316c51d74ba0ba905f289997ec05944c1681
ndfsa <25588077+ndfsa@users.noreply.github.com>
2023-09-10 23:53:51 -0400

    changes to lualine

--- .config/nvim/lua/plugin/lualine.lua
+++ .config/nvim/lua/plugin/lualine.lua
@@ -26,29 +26,22 @@ require("lualine").setup({
         globalstatus = true,
     },
     sections = {
-        lualine_a = {
-            "mode",
-        },
-        lualine_b = {
-            "filesize",
-        },
+        lualine_a = { "mode" },
+        lualine_b = { "filesize" },
         lualine_c = {
-            "branch",
-            "progress",
             {
                 "diagnostics",
                 sources = { "nvim_diagnostic" },
             },
-            function()
-                return "%t%4m%5r%4w"
-            end,
+            "filename",
+            "branch",
         },
         lualine_x = {
+            "progress",
             "location",
             function()
                 return "0x%02B"
             end,
-            "encoding",
         },
         lualine_y = {
             "filetype",
