7627054100f95d06f06dde9375137052f6510e00
ndfsa <25588077+ndfsa@users.noreply.github.com>
2023-10-14 15:25:17 -0400

    changes to zk notes

--- .config/nvim/lua/plugin/zk.lua
+++ .config/nvim/lua/plugin/zk.lua
@@ -1,1 +1,9 @@
-require("zk").setup()
+local opts = require("utils").opts
+
+local zk = require("zk")
+
+zk.setup()
+
+vim.keymap.set("n" , "<leader>Jd", function()
+    zk.new({ dir = "journal/daily" })
+end, opts("Journal daily"))
