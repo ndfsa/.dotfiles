da6a80dddcac9c74ff6b158c11b38ea430e0e6a0
ndfsa <25588077+ndfsa@users.noreply.github.com>
2024-01-13 19:35:56 -0400

    changes to plugins

--- .config/nvim/lua/plugin/lualine.lua
+++ .config/nvim/lua/plugin/lualine.lua
@@ -1,18 +1,7 @@
 local oil = {
     sections = {
         lualine_c = {
-            function()
-                local buff_name = vim.api.nvim_buf_get_name(0)
-                local prefix = "oil://"
-                local cwd = vim.fn.getcwd()
-                local name = cwd:match("^.*/(.*)")
-
-                if #buff_name < #cwd then
-                    return name .. "/" .. buff_name:sub(#prefix + 2)
-                else
-                    return name .. "/" .. buff_name:sub(#prefix + #cwd + 2)
-                end
-            end,
+            require("oil").get_current_dir,
         },
         lualine_d = { "branch" },
     },
--- .config/nvim/lua/plugin/luasnip.lua
+++ .config/nvim/lua/plugin/luasnip.lua
@@ -1,2 +1,16 @@
 require("luasnip.loaders.from_snipmate").lazy_load()
 local luasnip = require("luasnip")
+
+local opts = require("utils").opts
+
+vim.keymap.set({ "i", "s" }, "<C-E>", function()
+    if luasnip.choice_active() then
+        luasnip.change_choice(1)
+    end
+end, opts("Snippet change active choice"))
+vim.keymap.set({ "i", "s" }, "<C-L>", function()
+    luasnip.jump(1)
+end, opts("Snippet jump forward"))
+vim.keymap.set({ "i", "s" }, "<C-J>", function()
+    luasnip.jump(-1)
+end, opts("Snippet jump backward"))
