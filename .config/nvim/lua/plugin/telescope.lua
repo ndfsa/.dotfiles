7ba90665681151ace44782aab95647928d74d42d
ndfsa <25588077+ndfsa@users.noreply.github.com>
2024-01-22 00:49:02 -0400

    change the behavior of vimgrep

--- .config/nvim/lua/plugin/telescope.lua
+++ .config/nvim/lua/plugin/telescope.lua
@@ -1,3 +1,20 @@
+local vimgrep_arguments = {
+    "rg",
+    "--color=never",
+    "--hidden",
+    "--no-heading",
+    "--with-filename",
+    "--line-number",
+    "--column",
+    "--smart-case",
+    "--trim",
+}
+
+if vim.uv.fs_stat(".gitignore") then
+    table.insert(vimgrep_arguments, "--ignore-file")
+    table.insert(vimgrep_arguments, ".gitignore")
+end
+
 local telescope = require("telescope")
 local ignore_patterns = { "%.git[/\\]" }
 
@@ -9,6 +26,7 @@ telescope.setup({
             prompt_position = "top",
             mirror = true,
         },
+        vimgrep_arguments = vimgrep_arguments,
         preview = true,
         file_ignore_patterns = ignore_patterns,
     },
@@ -22,7 +40,22 @@ local tl_ext = telescope.extensions
 local tl_builtin = require("telescope.builtin")
 local opts = require("utils").opts
 
-vim.keymap.set("n", "<leader><space>", tl_builtin.find_files, opts("Find file"))
+local is_inside_work_tree = {}
+
+local function project_files()
+    local cwd = vim.fn.getcwd()
+    if is_inside_work_tree[cwd] == nil then
+        vim.fn.system("git rev-parse --is-inside-work-tree")
+        is_inside_work_tree[cwd] = vim.v.shell_error == 0
+    end
+    if is_inside_work_tree[cwd] then
+        tl_builtin.git_files({})
+    else
+        tl_builtin.find_files({})
+    end
+end
+
+vim.keymap.set("n", "<leader><space>", project_files, opts("Find file"))
 vim.keymap.set("n", "<leader>bz", tl_builtin.current_buffer_fuzzy_find, opts("Buffer fuzzy find"))
 vim.keymap.set("n", "<leader>fg", tl_builtin.live_grep, opts("Find grep"))
 vim.keymap.set("n", "<leader>fr", tl_builtin.registers, opts("Find registers"))
