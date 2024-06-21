9d5a1da567d87b1c36d51575482a7161526d378e
ndfsa <25588077+ndfsa@users.noreply.github.com>
2024-06-20 23:59:52 -0400

    update comic script

--- bin/convert_comic.sh
+++ bin/convert_comic.sh
@@ -6,8 +6,45 @@
 # get the directory filename
 file=$(basename "$1")
 
-# check if the directory exists
+# check if the file/directory exists
 [[ ! -e "./$file" ]] && exit 2
 
-# convert directory into a comic book file
-zip -9 -r "$file.cbz" "./$file"
+if [[ -d "./$file" ]]
+then
+    echo "processing directory $file"
+
+    # convert all images into jpgXL
+    find "./$file" -type f | parallel -j8 "magick mogrify -format jxl {1}"
+
+    # remove not jxl files
+    find "./$file" -type f -not -name "*.jxl" -delete
+
+    # convert directory into a comic book file
+    zip -rjm "$file.cbz" "./$file"
+
+    # remove directory
+    rm -rd "$file"
+else
+    echo "processing file $file"
+
+    # temporary directory
+    dir="${file%.*}"
+
+    # extract zip file
+    unzip "$file" -d "$dir"
+
+    # remove original
+    rm "$file"
+
+    # convert all images into jpgXL
+    find "./$dir" -type f | parallel -j8 "magick mogrify -format jxl {1}"
+
+    # remove not jxl files
+    find "./$dir" -type f -not -name "*.jxl" -delete
+
+    # convert directory into a comic book file
+    zip -rjm "$file" "./$dir"
+
+    # remove directory
+    rm -rd "$dir"
+fi
