9b474b9ac4dbfb766fcb3ec3bf30f4c2ca6121ce
ndfsa <25588077+ndfsa@users.noreply.github.com>
2025-02-02 15:12:22 -0400

    new emacs config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,102 +1,105 @@
-; clean up gui
-(setq inhibit-startup-message t)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(set-fringe-mode 10)
-(menu-bar-mode -1)
+;; The default is 800 kilobytes. Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
 
-; make semicolon work like in vim
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;; Include package manager
+(load-file "~/.config/emacs/package-manager.el")
 
-; set font
-(set-face-attribute 'default nil :font "JetBrainsMono-14:Medium")
-
-; set frame size
-(when window-system (set-frame-size (selected-frame) 120 36))
-
-; allow frame using fractional characters
-(setq frame-resize-pixelwise t)
-
-; scroll margin before cursor is at the end
-(setq scroll-margin 5)
-(setq scroll-step 1)
-
-(set-language-environment "UTF-8")
-(set-default-coding-systems 'utf-8)
-
-; bootstrap straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-; register use-package macro
-(straight-use-package 'use-package)
-
-; assume :straight t is the default
-(setq straight-use-package-by-default t)
-
-; packages
-(use-package dracula-theme
+(use-package gruvbox-theme
   :config
-  (load-theme 'dracula t))
-(use-package mood-line
-  :config
-  (mood-line-mode 1))
-(use-package goto-chg)
+  (load-theme 'gruvbox-dark-medium t))
+
+;; Set font face
+(set-face-attribute 'default nil
+  :font "JetBrains Mono"
+  :height 120
+  :weight 'medium)
+
 (use-package evil
-  :requires
-  (goto-chg)
   :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-w-delete t)
-  (setq evil-want-Y-yank-to-eol t)
-  (setq evil-undo-system 'undo-redo)
-  :config
-  (evil-mode 1))
+  (evil-mode)
+  :custom
+  (evil-want-keybinding nil)
+  (evil-want-C-u-scroll t)
+  (evil-want-C-i-jump t)
+  (evil-undo-system 'undo-redo))
+
 (use-package evil-collection
   :after evil
-  :config
-  (evil-collection-init))
-(use-package ivy
-  :config
-  (setq ivy-re-builders-alist
-    '((t . ivy--regex-fuzzy)))
-  (ivy-mode 1))
-(use-package counsel
-  :config
-  (counsel-mode 1))
-(use-package ivy-rich
-  :config
-  (ivy-rich-mode 1))
-(use-package general)
-(use-package projectile
-  :config
-  (setq projectile-completion-system 'ivy))
-(use-package tree-sitter)
-(use-package tree-sitter-langs)
-(global-tree-sitter-mode)
-(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+  :config (evil-collection-init))
 
-(use-package rust-mode)
-(use-package lsp-mode
+(use-package vertico
+  :custom
+  (vertico-scroll-margin 0)
+  (vertico-count 20)
+  (vertico-cycle t)
   :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((rust-mode . lsp))
-  :commands lsp)
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package org :straight nil)
-(use-package magit)
-(use-package ediff)
+  (vertico-mode))
+
+(use-package savehist :init (savehist-mode))
+(use-package marginalia :init (marginalia-mode))
+(use-package consult
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+  :init
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+  :config
+  (consult-customize
+    consult-theme :preview-key '(:debounce 0.2 any)
+    consult-ripgrep consult-git-grep consult-grep consult-man
+    consult-bookmark consult-recent-file consult-xref
+    consult--source-bookmark consult--source-file-register
+    consult--source-recent-file consult--source-project-recent-file
+    :preview-key '(:debounce 0.4 any))
+
+  (setq consult-narrow-key "<"))
+
+(use-package emacs
+  :ensure nil
+  :custom
+  (scroll-bar-mode nil)
+  (tool-bar-mode nil)
+  (tooltip-mode nil)
+  (menu-bar-mode nil)
+  (electric-indent-mode nil)
+  (make-backup-files nil)
+  (auto-save-default nil)
+
+  (set-fringe-mode 10)
+  (inhibit-startup-message t)
+  (global-auto-revert-mode t)
+  (electric-pair-mode t)
+  (scroll-margin 5)
+  (scroll-step 1)
+  (frame-resize-pixelwise t)
+  (global-display-line-numbers-mode t)
+  (mouse-wheel-progressive-speed nil)
+  (scroll-conservatively 10)
+  (enable-recursive-minibuffers t)
+  (read-extended-command-predicate #'command-completion-default-include-p)
+
+  (straight-use-package-by-default t)
+
+  (current-language-environment "UTF-8")
+  
+  :init
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  :hook
+  (minibuffer-setup . cursor-intangible-mode)
+
+  :bind
+  (( [escape] . keyboard-escape-quit)))
+
+(setq gc-cons-threshold (* 2 1000 1000))
+(setq read-process-output-max (* 1024 1024))
